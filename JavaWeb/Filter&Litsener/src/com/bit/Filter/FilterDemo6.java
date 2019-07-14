d0a249cdb9ceefb278335b7363bb2e54529d6a02
jinligithub <1018487750@qq.com>
2019-07-14 21:44:33 +0800

    过路器执行流程

---
+++ JavaWeb/Filter&Litsener/src/com/bit/Filter/FilterDemo6.java
@@ -0,0 +1,23 @@
+package com.bit.Filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class FilterDemo6 implements Filter {
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        System.out.println("filterDemo6执行了。。");
+        chain.doFilter(req, resp);
+        System.out.println("filterDemo6回来了");
+    }
+
+    public void destroy() {
+    }
+
+}
